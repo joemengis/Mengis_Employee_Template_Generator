eb405ba6971723d7456e64dd239410c8e52498d7
LJFrancis <joe.mengis@gmail.com>
2020-07-20 22:17:30 -0700

    finished homework

--- Develop/app.js
+++ Develop/app.js
@@ -10,6 +10,133 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const teamMembers = [];
+
+async function init() {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name:'name',
+            message:'Enter the manager\'s name: ',
+            validate: function(name) {
+                if(name.trim() === '') {
+                    console.log('Name is required.')
+                    return false;
+                }
+
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name:'id',
+            message:'Enter the manager\'s id: ',
+        },
+        {
+            type: 'input',
+            name:'email',
+            message:'Enter the manager\'s email: ',
+        },
+        {
+            type: 'input',
+            name:'officeNumber',
+            message:'Enter the manager\'s office number: ',
+        },
+    ]);
+
+    const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+    teamMembers.push(manager);
+    determineChoice();
+}
+ init();
+
+ async function determineChoice() {
+    //  ask whether they want to add and intern, engineer or done
+    const answers = await inquirer.prompt({
+        type: 'list',
+        choices:['intern', 'engineer', 'done'],
+        name: 'choice',
+        message: 'Select one of the following: ',
+    })
+
+    if(answers.choice === 'intern') {
+        createIntern();
+        
+    }
+    if(answers.choice === 'engineer') {
+        createEngineer();
+        
+    }
+    if (answers.choice === 'done') {
+        createHTML();
+    }
+ }
+
+ async function createIntern() {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name:'name',
+            message:'Enter the Intern\'s name: ',
+        },
+        {
+            type: 'input',
+            name:'id',
+            message:'Enter the Intern\'s id: ',
+        },
+        {
+            type: 'input',
+            name:'email',
+            message:'Enter the Intern\'s email: ',
+        },
+        {
+            type: 'input',
+            name:'school',
+            message:'Enter the intern\'s school: ',
+        },
+    ]);
+
+    const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+    teamMembers.push(intern);
+    createHTML();
+ }
+
+ async function createEngineer() {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name:'name',
+            message:'Enter the Engineer\'s name: ',
+        },
+        {
+            type: 'input',
+            name:'id',
+            message:'Enter the Engineer\'s id: ',
+        },
+        {
+            type: 'input',
+            name:'email',
+            message:'Enter the Engineer\'s email: ',
+        },
+        {
+            type: 'input',
+            name:'github',
+            message:'Enter the Engineer\'s GitHub: ',
+        },
+    ]);
+
+    const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+    teamMembers.push(engineer);
+    createHTML();
+}
+
+async function createHTML() {
+    console.log('output path is: ' , outputPath);
+    console.log('team members are: ', teamMembers);
+    fs.writeFileSync(outputPath, render(teamMembers));
+}
+
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
